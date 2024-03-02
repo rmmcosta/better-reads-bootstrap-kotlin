49dc0627849f1cf3d04a7c9e2705ae903c12b547
rmmcosta <ricardocosta101085@gmail.com>
2024-03-02 16:02:57 +0000

    feat: book definition

---
+++ src/main/kotlin/com/rmmcosta/betterreadskotlinbootstrap/book/Book.kt
@@ -0,0 +1,40 @@
+package com.rmmcosta.betterreadskotlinbootstrap.book
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType
+import org.springframework.data.cassandra.core.mapping.CassandraType
+import org.springframework.data.cassandra.core.mapping.Column
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn
+import org.springframework.data.cassandra.core.mapping.Table
+import java.time.LocalDate
+
+@Table(value = "book_by_id")
+data class Book(
+    @Id @PrimaryKeyColumn(name = "book_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    var id: String,
+
+    @Column("book_name")
+    @CassandraType(type = CassandraType.Name.TEXT)
+    var name: String,
+
+    @Column("book_description")
+    @CassandraType(type = CassandraType.Name.TEXT)
+    var description: String,
+
+    @Column("book_published_date")
+    @CassandraType(type = CassandraType.Name.DATE)
+    var publishedDate: LocalDate,
+
+    @Column("book_cover_ids")
+    @CassandraType(type = CassandraType.Name.LIST, typeArguments = [CassandraType.Name.BIGINT])
+    var covers: List<Long>,
+
+    @Column("book_author_names")
+    @CassandraType(type = CassandraType.Name.LIST, typeArguments = [CassandraType.Name.TEXT])
+    var authorNames: List<String>,
+
+    @Column("book_author_ids")
+    @CassandraType(type = CassandraType.Name.LIST, typeArguments = [CassandraType.Name.TEXT])
+    var authorIds: List<String>,
+
+)
