9ab3df0c1593f7493d9d576530a98f303b8a3f72
rmmcosta <ricardocosta101085@gmail.com>
2024-03-11 08:27:35 +0000

    fix: column names

--- src/main/kotlin/com/rmmcosta/betterreadskotlinbootstrap/author/Author.kt
+++ src/main/kotlin/com/rmmcosta/betterreadskotlinbootstrap/author/Author.kt
@@ -4,7 +4,6 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType
 import org.springframework.data.cassandra.core.mapping.CassandraType
 import org.springframework.data.cassandra.core.mapping.CassandraType.Name
-import org.springframework.data.cassandra.core.mapping.Column
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn
 import org.springframework.data.cassandra.core.mapping.Table
 
@@ -13,11 +12,9 @@ class Author(
     @Id @PrimaryKeyColumn(name = "author_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
     var id: String,
 
-    @Column(value = "author_name")
     @CassandraType(type = Name.TEXT)
     var name: String?,
 
-    @Column(value = "personal_name")
     @CassandraType(type = Name.TEXT)
     var personalName: String?,
 )
--- src/main/kotlin/com/rmmcosta/betterreadskotlinbootstrap/book/Book.kt
+++ src/main/kotlin/com/rmmcosta/betterreadskotlinbootstrap/book/Book.kt
@@ -3,7 +3,6 @@ package com.rmmcosta.betterreadskotlinbootstrap.book
 import org.springframework.data.annotation.Id
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType
 import org.springframework.data.cassandra.core.mapping.CassandraType
-import org.springframework.data.cassandra.core.mapping.Column
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn
 import org.springframework.data.cassandra.core.mapping.Table
 import java.time.LocalDate
@@ -13,27 +12,21 @@ data class Book(
     @Id @PrimaryKeyColumn(name = "book_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
     var id: String,
 
-    @Column(value = "book_name")
     @CassandraType(type = CassandraType.Name.TEXT)
     var name: String,
 
-    @Column(value = "book_description")
     @CassandraType(type = CassandraType.Name.TEXT)
     var description: String,
 
-    @Column(value = "book_published_date")
     @CassandraType(type = CassandraType.Name.DATE)
     var publishedDate: LocalDate,
 
-    @Column(value = "book_cover_ids")
     @CassandraType(type = CassandraType.Name.LIST, typeArguments = [CassandraType.Name.BIGINT])
     var covers: List<Long>,
 
-    @Column(value = "book_author_names")
     @CassandraType(type = CassandraType.Name.LIST, typeArguments = [CassandraType.Name.TEXT])
     var authorNames: List<String>,
 
-    @Column(value = "book_author_ids")
     @CassandraType(type = CassandraType.Name.LIST, typeArguments = [CassandraType.Name.TEXT])
     var authorIds: List<String>,
 
